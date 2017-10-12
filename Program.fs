cd953572a9d6648a9e3858c27230a8c90ff693b1
Theodore Tsirpanis <teo.tsirpanis.718@gmail.com>
2017-10-12 13:32:40 +0300

    Added macOS, and .NET Core builds. Rank numbers are optional.

--- Program.fs
+++ Program.fs
@@ -19,11 +19,13 @@ type PrefSortExiter() =
 type Arguments =
     | [<Unique; AltCommandLine("-i")>] InputFile of string
     | [<Unique; AltCommandLine("-o")>] OutputFile of string
+    | [<Unique>] NoNumbers
     interface IArgParserTemplate with
         member x.Usage =
             match x with
             | InputFile _ -> "The file that contains the items to be sorted."
             | OutputFile _ -> "The file that will contain the sorted items. Writes to stdout if not specified."
+            | NoNumbers -> "Numbers will not be prepended to the output."
 
 let curry f x1 x2 = f(x1, x2)
 
@@ -32,14 +34,16 @@ let uncurry f (x1, x2) = f x1 x2
 let rec prefSort x1 x2 =
     eprintf "Do You prefer %s (1), or %s (2)? Write your answer: " x1 x2
     match Console.ReadLine() with
-    | "0" ->
-        eprintfn "Can't decide, right? We have lots of things in common! :-p"
-        0
     | "1" -> -1
     | "2" -> 1
     | _ ->
         eprintfn "Invalid answer. :-("
         prefSort x1 x2
+
+let addNumbers =
+    function
+    | true -> Seq.indexed >> Seq.map (fun (c, s) -> sprintf "%d. %s" (c + 1) s)
+    | false -> id
 
 [<EntryPoint>]
 let main argv =
@@ -47,13 +51,13 @@ let main argv =
     let results = parser.ParseCommandLine()
     let inputFile = results.GetResult (<@ InputFile @>, defaultValue = "input.txt")
     let outputFile = results.GetResult (<@ OutputFile @>, defaultValue = "output.txt")
+    let shouldAdd = results.Contains <@ NoNumbers @>
 
     inputFile
     |> File.ReadAllLines
     |> Seq.filter (String.IsNullOrWhiteSpace >> not)
     |> Seq.sortWith prefSort
-    |> Seq.indexed
-    |> Seq.map (fun (c, s) -> sprintf "%d: %s" (c + 1) s)
+    |> addNumbers shouldAdd
     |> curry File.WriteAllLines outputFile
 
     printfn "Success. Check out the results in %s." outputFile
--- build.ps1
+++ build.ps1
@@ -2,8 +2,14 @@ dotnet restore
 
 Remove-Item prefsort-* -recurse
 
-dotnet publish prefsort.fsproj -c Release -f net45 --self-contained -r win-x86 -o .\prefsort-win32
-Compress-Archive -Path ".\prefsort-win32" -DestinationPath ".\prefsort-win32"
+dotnet publish prefsort.fsproj -c Release -f net45 -r win-x86 -o .\prefsort-windows
+Compress-Archive -Path ".\prefsort-windows" -DestinationPath ".\prefsort-windows"
 
-dotnet publish prefsort.fsproj -c Release -f netcoreapp2.0 --self-contained -r linux-x64 -o .\prefsort-linux
+dotnet publish prefsort.fsproj -c Release -f netcoreapp2.0 -r linux-x64 -o .\prefsort-linux
 Compress-Archive -path ".\prefsort-linux" -DestinationPath ".\prefsort-linux"
+
+dotnet publish prefsort.fsproj -c Release -f netcoreapp2.0 -r osx-x64 -o .\prefsort-mac
+Compress-Archive -path ".\prefsort-mac" -DestinationPath ".\prefsort-mac"
+
+dotnet publish prefsort.fsproj -c Release -f netcoreapp2.0 -o .\prefsort-netcore
+Compress-Archive -path ".\prefsort-netcore" -DestinationPath ".\prefsort-netcore"
